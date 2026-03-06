963f5b5103fdb1c969b8cfe9a94670bf5a8e50ba
ivanGG23 <243836@ids.upchiapas.edu.mx>
2026-03-06 14:44:09 -0600

    fix: new

--- js/api.js
+++ js/api.js
@@ -1,21 +1,19 @@
-const BASE_URL = CONFIG.API_BASE_URL;
-
 const getGatosRandom = async (limit = 10) => {
-    const response = await fetch(`${BASE_URL}/random?limit=${limit}`);
+    const response = await fetch(`${BASE_URL}/cats/random?limit=${limit}`);
     const json = await response.json();
     if (!json.success) throw new Error(json.message);
     return json.data;
 };
 
 const getRazas = async () => {
-    const response = await fetch(`${BASE_URL}/breeds`);
+    const response = await fetch(`${BASE_URL}/cats/breeds`);
     const json = await response.json();
     if (!json.success) throw new Error(json.message);
     return json.data;
 };
 
 const getGatosPorRaza = async (breedId, limit = 10) => {
-    const response = await fetch(`${BASE_URL}/breed/${breedId}?limit=${limit}`);
+    const response = await fetch(`${BASE_URL}/cats/breed/${breedId}?limit=${limit}`);
     const json = await response.json();
     if (!json.success) throw new Error(json.message);
     return json.data;
